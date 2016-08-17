a9860f4993c68f034d7b5759981fb89aef68f024
ozthekoder <oozdemir2704@gmail.com>
2016-08-17 16:46:26 -0400

    some improvements for dictionary

--- src/actions/home.js
+++ src/actions/home.js
@@ -2,56 +2,77 @@ import * as types from '../constants/actions';
 import mammoth from 'mammoth';
 import Promise from 'bluebird';
 import React from 'react';
+const reader = new window.FileReader();
 
 export function readFile(e) {
   return (dispatch) => {
-    return new Promise((resolve) => {
-      const reader = new window.FileReader();
-      reader.onload = () => {
-        resolve({ arrayBuffer: reader.result});
-      };
-      reader.readAsArrayBuffer(e.target.files[0]);
-    })
-    .then(mammoth.extractRawText)
-    .then((result) => {
-      console.log(result.value);
-      const text = result.value;
-      const dictionary = {};
-      const refs = {};
-      const content = [];
-      const lines = text
-      .split('\n')
-      .filter((line => !!line))
-      .filter(line => /((([A-Z])([0-9])+)+(\/)*(([A-Z])*([0-9])+)*)/g.test(line))
-      .map((line) => line.split(/((([A-Z])([0-9])+)+(\/)*(([A-Z])*([0-9])+)*)/g))
-      .map((split) => {
-        return {
-          ref: split[1],
-          text: split[split.length-1].split('/').map((passage) => passage.trim())
-        };
-      });
-
-      lines.forEach((p) => {
-        if(refs[p.ref]) {
-          let index = Number(p.ref.substring(1));
-          index = index + 1;
-          p.ref = `S${index}`;
-        }
-
-        refs[p.ref] = true;
-        content.push(<p key={p.ref}>{p.text}</p>)
-        const words = p.text[0].split(' ').concat(p.text[1].split(' '));
-        words.forEach((w) => {
-          dictionary[w] = dictionary[w] || [];
-          dictionary[w].push(p.ref);
-        });
-      });
-      console.log(JSON.stringify(dictionary, null, 2));
-
+    return readUploadedFile(e.target.files[0])
+    .then(extractText)
+    .then(extractLines)
+    .then(createDictionary)
+    .then((payload) => {
       dispatch({
         type: types.SET_CONTENTS,
-        payload: content
+        payload
       });
     });
+  }
+}
+
+function readUploadedFile(file) {
+  return new Promise((resolve) => {
+    reader.onload = () => { resolve(reader.result) };
+    reader.readAsArrayBuffer(file);
+  });
+}
+
+function extractText(arrayBuffer) {
+  return mammoth.extractRawText({ arrayBuffer });
+}
+
+function extractLines(result) {
+  const text = result.value;
+  const dictionary = {};
+  const refs = {};
+  const content = [];
+  return text
+  .split('\n')
+  .filter((line => !!line))
+  .filter(line => /((([A-Z])([0-9])+)+(\/)*(([A-Z])*([0-9])+)*)/g.test(line))
+  .map((line) => line.split(/((([A-Z])([0-9])+)+(\/)*(([A-Z])*([0-9])+)*)/g))
+  .map((split) => {
+    return {
+      ref: split[1],
+      text: split[split.length-1].split('/').map((passage) => passage.trim())
+    };
+  });
+
+}
+
+function createDictionary(lines) {
+  const dictionary = {};
+  const refs = {};
+  const contents = [];
+
+  lines.forEach((p) => {
+    if(refs[p.ref]) {
+      let index = Number(p.ref.substring(1));
+      index = index + 1;
+      p.ref = `S${index}`;
+    }
+
+    refs[p.ref] = true;
+    contents.push(<p key={p.ref}>{p.text}</p>)
+    const words = p.text[0].split(' ').concat(p.text[1].split(' '));
+    words.forEach((w) => {
+      dictionary[w] = dictionary[w] || [];
+      dictionary[w].push(p.ref);
+    });
+  });
+
+  return {
+    dictionary,
+    contents,
+    lines
   }
 }
--- src/components/Home.js
+++ src/components/Home.js
@@ -3,12 +3,24 @@ import React, { PropTypes } from 'react';
 const HomePage = (props) => {
   return (
     <div className="home-page">
+    <div className="toolbox">
+    <div className="clearfix">
+    <div className="column">
     <label htmlFor="fileinput">
       <input onChange={props.readFile} name="fileinput" type="file" id="fileinput" />
       Docx Yukle
     </label>
-    <div>
+    </div>
+    <div className="column">
+    </div>
+    </div>
+    </div>
+    <div className="clearfix">
+    <div className="column">
       {props.contents}
+    </div>
+    <div className="column">
+    </div>
     </div>
     </div>
   );
